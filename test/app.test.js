30f99c2d1184c7bcc28e1b7dc88c9973af5955b5
YoProgrammer <yuttasakcom@gmail.com>
2018-03-05 00:06:28 +0700

    create test

--- test/app.test.js
+++ test/app.test.js
@@ -4,7 +4,7 @@ const request = require('supertest')
 const server = require('../src/server')
 
 describe('routes: /api', () => {
-  it('should respond as expected', (done) => {
+  it('should respond as expected', async done => {
     request(server)
       .get('/api')
       .expect('Get Api')
