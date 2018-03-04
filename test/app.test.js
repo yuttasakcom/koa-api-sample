3b7ad7d470af156d531725ebf0430db2bb05a716
YoProgrammer <yuttasakcom@gmail.com>
2018-03-05 00:15:40 +0700

    update test/app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,12 @@
 /* global describe it */
 const request = require('supertest')
+const expect = require('expect')
 
 const server = require('../src/server')
 
 describe('routes: /api', () => {
-  it('should respond as expected', async done => {
-    request(server)
-      .get('/api')
-      .expect('Get Api')
-      .end(done)
+  it('should respond as expected', async () => {
+    const response = await request(server).get('/api')
+    expect(response.status).toEqual(200)
   })
 })
