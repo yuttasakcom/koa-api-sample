231dbb6642c8b0bac88eeb993bf5b13c49f971cb
YoProgrammer <yuttasakcom@gmail.com>
2018-03-04 14:46:09 +0700

    create authentication middleware

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,4 +1,4 @@
-module.exports = (token) => (ctx, next) => {
+module.exports = token => (ctx, next) => {
   // Authentication handler
   if (token === 'token') {
     console.log('auth middleware started')
