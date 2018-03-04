1f8518c0175e2a01a1577b8185c6315eae13b5f6
YoProgrammer <yuttasakcom@gmail.com>
2018-03-05 00:37:37 +0700

    update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -5,9 +5,8 @@ const expect = require('expect')
 const server = require('../src/server')
 
 describe('routes: /api', () => {
-  it('should return Get Api body', async done => {
+  it('should return Get Api body', async () => {
     const response = await request(server).get('/api')
     expect(response.status).toEqual(200)
-    done()
   })
 })
