3fda6e5faa3c068f842f34ebf2c4450009efc9ff
YoProgrammer <yuttasakcom@gmail.com>
2018-03-04 14:12:37 +0700

    create admin router

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,6 @@
-const getApi = (ctx, next) => { ctx.body = 'Get Api' }
-const getById = (ctx, next) => { ctx.body = `Get by id:${ctx.params.id}` }
-const postApi = (ctx, next) => { ctx.body = 'Post Api' }
+const getApi = (ctx) => { ctx.body = 'Get Api' }
+const getById = (ctx) => { ctx.body = `Get by id:${ctx.params.id}` }
+const postApi = (ctx) => { ctx.body = 'Post Api' }
 
 const adminRouter = router => router
   .get('/api', getApi)
