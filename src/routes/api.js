bc8aebfc9325d49277eae9ff9d12ee1148893a2e
YoProgrammer <yuttasakcom@gmail.com>
2018-03-05 10:51:01 +0700

    create server test

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,8 @@
 const auth = require('../middleware/authentication')
 
-const getApi = (ctx) => { ctx.body = 'Get Api' }
-const getById = (ctx) => { ctx.body = `Get by id:${ctx.params.id}` }
-const postApi = (ctx) => { ctx.body = 'Post Api' }
+const getApi = async ctx => { ctx.body = 'Get Api' }
+const getById = async ctx => { ctx.body = `Get by id:${ctx.params.id}` }
+const postApi = async ctx => { ctx.body = 'Post Api' }
 
 const adminRouter = router => router
   .get('/api', auth('token'), getApi)
